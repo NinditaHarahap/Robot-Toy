0a76e523ca86598f975425a4955969c9cfba1884
Nindita Harahap <nindita.harahap@gmail.com>
2018-07-28 02:26:31 +0800

    Update Test_Solution.py

--- Test_Solution.py
+++ Test_Solution.py
@@ -1,5 +1,27 @@
 import Solution
 
 def test_input():
-  pass
+  InputValue = "PLACE 0,0,NORTH"
+  OrderValue = "MOVE"
+  OrderValue = "REPORT"
+  assert print(X, Y, F) == '0,1,NORTH'
+
+  InputValue = "PLACE 0,0,NORTH"
+  OrderValue = "LEFT"
+  OrderValue = "REPORT"
+  assert print(X, Y, F) == '0,0,WEST'
+
+  InputValue = "PLACE 0,0,NORTH"
+  OrderValue = "LEFT"
+  OrderValue = "REPORT"
+  assert print(X, Y, F) == '0,0,WEST'
+
+  InputValue = "PLACE 1,2,EAST"
+  OrderValue = "MOVE"
+  OrderValue = "MOVE"
+  OrderValue = "LEFT"
+  OrderValue = "MOVE"
+  OrderValue = "REPORT"
+  assert print(X, Y, F) == '3,3,NORTH'
+
   
