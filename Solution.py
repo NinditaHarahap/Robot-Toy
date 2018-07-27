645a2cc537c5c1b60e173173bb45c426e7b48e4c
Nindita Harahap <nindita.harahap@gmail.com>
2018-07-28 00:19:47 +0800

    Update Solution.py

--- Solution.py
+++ Solution.py
@@ -1,64 +1,66 @@
-class GetPosition(object):
+class GetAttributes(object):
     def __init__(self, PLACE, X, Y, F, ORDER):
-         self.PLACE = PLACE
-         self.X = X
-         self.Y = Y
-         self.F = F
-         self.ORDER = ORDER
+        self.PLACE = PLACE
+        self.X = X
+        self.Y = Y
+        self.F = F
+        self.ORDER = ORDER
     
-    def GetInputPosition(self):
-        while True:
-          try:
-            value = input("Where Should I Go: ").upper()
-            if value.split(' ', 1)[0] == 'PLACE'
-                continue
-            else:
-                
-          
-          if len(value) > 
-          if len(input("Where Should I Go: ")>
-            X, Y = [int(x), int(y)]
-
-'''
-    def get_non_negative_int(prompt):
-        while True:
-            try:
-                value = int(input(prompt))
-            except ValueError:
-                print("Sorry, I didn't understand that.")
-                continue
-
-            if value < 0:
-                print("Sorry, your response must not be negative.")
-                continue
-            else:
-                break
-        return value
-OR
-
-var1, var2, var3 = [x for x in input("Enter your command: ").str.upper.split()]
-
-'''
-
-    def InitialPost(self):
-        if PLACE():
-            x = x
-            y = y
-            FACE = FACE
+    def GetInputValue(self):
+        InputValue = input("Where Should I Go: ")
+        OrderValue = InputValue.upper().split(' ', 1)[0]
+        if OrderValue == 'PLACE':
+            Position = InputValue.replace(',',' ').split()
+            X = max(5, min(Position[1], Position[1]))
+            Y = max(5, min(Position[1], Position[1]))
+            F = Position[3]
         else:
-            input('You haven\'t put your command. Please type \'PLACE,  X, Y, FACE\'':)
+            ORDER = OrderValue
 
-    def Direction(self):
-        {'NORTH':y+=1, 'EAST':x+=1, 'WEST':x-=1, 'SOUTH':y-=1}
+    def MakeMove(self):
+        {'NORTH':Y+=1, X=X, 
+         'EAST':X+=1, Y=Y,
+         'WEST':X-=1, Y=Y,
+         'SOUTH':Y-=1, X=X}
 
     def TurnRight(self):
-        {'NORTH':'EAST', 'EAST':'SOUTH', 'WEST':'NORTH', 'SOUTH':'WEST'}
+        {'NORTH':'EAST', 
+         'EAST':'SOUTH', 
+         'WEST':'NORTH', 
+         'SOUTH':'WEST'}
 
     def TurnLeft(self):
-        {'NORTH':'WEST', 'EAST':'NORTH', 'WEST':'SOUTH', 'SOUTH':'EAST'}
+        {'NORTH':'WEST', 
+         'EAST':'NORTH', 
+         'WEST':'SOUTH', 
+         'SOUTH':'EAST'}
 
-    def RobotRun(self):
-        pass
-
-
-
+    def GetReport(self):
+        print(X, Y, F)
+    
+class RobotRun(object):
+    def __init__(self, GetAttributes):
+        self.GetAttributes = GetAttributes
+    
+    def RunRobotRun(self):
+        if self.OrderValue() != 'PLACE':
+            return InputValue
+        elif ORDER == 'PLACE':
+            X = GetInputValue.X
+            Y = GetInputValue.Y
+            F = GetInputValue.F
+            if ORDER == 'MOVE'
+                return X = X.MakeMove(), Y = Y.MakeMove()
+                continue
+            if ORDER == 'RIGHT':
+                return F = F.TurnRight()
+                continue
+            if ORDER == 'LEFT':
+                return F = F.TurnLeft()
+                continue
+            if ORDER == 'REPORT':
+                print(X, Y, F)
+           
+        
+if __name__ == '__main__':
+    main()
