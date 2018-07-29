5ac666b30f012ca1651853b37ee4ef60ab2843d5
Nindita Harahap <nindita.harahap@gmail.com>
2018-07-29 11:34:11 +0800

    Update Solution_Working.py

--- Solution_Working.py
+++ Solution_Working.py
@@ -7,9 +7,15 @@ def clamp(n, minn, maxn):
 	else:
 		return n
 	
-#Dictionary of Movement
+#Lists and Dictionaries
+minn = 0 
+maxn = 5
+limit = 7
+OrderList = ('LEFT','RIGHT','MOVE','REPORT')
+Direction = ('NORTH','EAST','WEST','SOUTH')
 TurnRight = {'NORTH':'EAST', 'EAST':'SOUTH', 'WEST':'NORTH', 'SOUTH':'WEST'}
 TurnLeft = {'NORTH':'WEST', 'EAST':'NORTH', 'WEST':'SOUTH', 'SOUTH':'EAST'}
+ErrorMessage = 'Hmmm.. Did you have a typo on your command? Check again please.'
 
 def MakeMove(X, Y, F):
     if F == 'NORTH':
@@ -20,28 +26,32 @@ def MakeMove(X, Y, F):
         X = clamp(X - 1, minn, maxn)
     elif F == 'SOUTH':
         Y = clamp(Y - 1, minn, maxn)
+    else:
+    	print('Hmmm.. Did you have a typo on your command? Check again please.')
     return X, Y, F
           
 #Operations
+print("Your command options: \nPLACE X, Y, (NORTH/EAST/SOUTH/WEST) --> Use this first\nRIGHT\nLEFT\nMOVE\nREPORT --> Get your current location")
 while True:
 	InputValue = input("What Do You Want To Do: ").upper()
-	minn = 0
-	maxn = 5
-	limit = 7
 	if InputValue == 'QUIT':
 		break
 	if len(InputValue) >= limit:
 		ORDER, X, Y, F = InputValue.strip().replace(',',' ').split()
-		if ORDER == 'PLACE':
-			X = clamp(int(X), minn, maxn)
-			Y = clamp(int(Y), minn, maxn)
+		if F in Direction:
+			if ORDER == 'PLACE':
+				X = clamp(int(X), minn, maxn)
+				Y = clamp(int(Y), minn, maxn)
+		else: print(ErrorMessage)
 	if len(InputValue) <= limit:
 		ORDER = InputValue.strip().upper()
-		if ORDER == 'LEFT':
-			F = TurnLeft[F]
-		if ORDER == 'RIGHT':
-			F = TurnRight[F]
-		if ORDER == 'MOVE':
-			X, Y, F = MakeMove(X, Y, F)
-		if ORDER == 'REPORT':
-			print('Your current coordinate is: ',X,Y,F)
+		if ORDER in OrderList:
+			if ORDER == 'LEFT':
+				F = TurnLeft[F]
+			if ORDER == 'RIGHT':
+				F = TurnRight[F]
+			if ORDER == 'MOVE':
+				X, Y, F = MakeMove(X, Y, F)
+			if ORDER == 'REPORT':
+				print('Your current coordinate is: ',X,Y,F)
+		else: print(ErrorMessage)
