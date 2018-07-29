86373dd64df5eef8a93c492b49daf59ccced6c2d
Nindita Harahap <nindita.harahap@gmail.com>
2018-07-29 13:51:20 +0800

    Add files via upload

--- Solution_Concept.py
+++ Solution_Concept.py
@@ -4,7 +4,7 @@ class GetAttributes(object):
         self.X = X
         self.Y = Y
         self.F = F
-        
+    
     def clamp(n, minn, maxn):
         if n < minn:
             return minn
@@ -24,37 +24,35 @@ class GetAttributes(object):
             Y = clamp(Y - 1, minn, maxn)
         return X, Y, F
 
-#Lists and Dictionaries
-OrderList = ('LEFT','RIGHT','MOVE','REPORT')
-Direction = ('NORTH','EAST','WEST','SOUTH')
 TurnRight = {'NORTH':'EAST', 'EAST':'SOUTH', 'WEST':'NORTH', 'SOUTH':'WEST'}
 TurnLeft = {'NORTH':'WEST', 'EAST':'NORTH', 'WEST':'SOUTH', 'SOUTH':'EAST'}
-minn = 0 
-maxn = 5
-limit = max(len(Or) for Or in OrderList)+1
-ErrorMessage = 'Hmmm.. Did you have a typo on your command? Check again please.'
+          
+    
+class RobotRun(object):
+    def __init__(self, GetAttributes):
+        self.GetAttributes = GetAttributes
     
     def RunRobotRun(self):
         while True:
             InputValue = input("What Do You Want To Do: ").upper()
-	        if InputValue == 'QUIT':
-		        break
-	        if len(InputValue) >= limit:
-		        ORDER, X, Y, F = InputValue.strip().replace(',',' ').split()
-		        if F in Direction:
-			        if ORDER == 'PLACE':
-				        X = clamp(int(X), minn, maxn)
-				        Y = clamp(int(Y), minn, maxn)
-		        else: print(ErrorMessage)
-	        if len(InputValue) <= limit:
-		        ORDER = InputValue.strip().upper()
-		        if ORDER in OrderList:
-			        if ORDER == 'LEFT':
-				        F = TurnLeft[F]
-			        if ORDER == 'RIGHT':
-				        F = TurnRight[F]
-			        if ORDER == 'MOVE':
-				        X, Y, F = MakeMove(X, Y, F)
-			        if ORDER == 'REPORT':
-				        print('Your current coordinate is: ',X,Y,F)
-		        else: print(ErrorMessage)
+            minn = 0
+            maxn = 5
+            limit = 7
+            if InputValue == 'QUIT':
+                break
+            if len(InputValue) >= limit:
+                ORDER, X, Y, F = InputValue.strip().replace(',',' ').split()
+            if ORDER == 'PLACE':
+                X = clamp(int(X), minn, maxn)
+                Y = clamp(int(Y), minn, maxn)
+            if len(InputValue) <= limit:
+                ORDER = InputValue.strip().upper()
+            if ORDER == 'LEFT':
+                F = TurnLeft[F]
+            if ORDER == 'RIGHT':
+                F = TurnRight[F]
+            if ORDER == 'MOVE':
+                X, Y, F = MakeMove(X, Y, F)
+            if ORDER == 'REPORT':
+                print('Your current coordinate is: ',X,',',Y,',',F)
+
