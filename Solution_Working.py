7ba5190f89648eb83d584d33491702ec77b88361
Nindita Harahap <nindita.harahap@gmail.com>
2018-07-29 12:46:54 +0800

    Update Solution_Working.py

--- Solution_Working.py
+++ Solution_Working.py
@@ -6,16 +6,6 @@ def clamp(n, minn, maxn):
 		return maxn
 	else:
 		return n
-	
-#Lists and Dictionaries
-minn = 0 
-maxn = 5
-limit = 7
-OrderList = ('LEFT','RIGHT','MOVE','REPORT')
-Direction = ('NORTH','EAST','WEST','SOUTH')
-TurnRight = {'NORTH':'EAST', 'EAST':'SOUTH', 'WEST':'NORTH', 'SOUTH':'WEST'}
-TurnLeft = {'NORTH':'WEST', 'EAST':'NORTH', 'WEST':'SOUTH', 'SOUTH':'EAST'}
-ErrorMessage = 'Hmmm.. Did you have a typo on your command? Check again please.'
 
 def MakeMove(X, Y, F):
     if F == 'NORTH':
@@ -29,11 +19,22 @@ def MakeMove(X, Y, F):
     else:
     	print(ErrorMessage)
     return X, Y, F
-          
-#Operations
-print('Your command options: \nPLACE X, Y, (NORTH/EAST/SOUTH/WEST) --> Use this command first\nRIGHT\nLEFT\nMOVE\nREPORT --> Get your current location')
+
+#Lists and Dictionaries
+OrderList = ('LEFT','RIGHT','MOVE','REPORT')
+Direction = ('NORTH','EAST','WEST','SOUTH')
+TurnRight = {'NORTH':'EAST', 'EAST':'SOUTH', 'WEST':'NORTH', 'SOUTH':'WEST'}
+TurnLeft = {'NORTH':'WEST', 'EAST':'NORTH', 'WEST':'SOUTH', 'SOUTH':'EAST'}
+ErrorMessage = 'Hmmm.. Did you have a typo on your command? Check again please.'
+minn = 0 
+maxn = 5
+limit = max(len(Or) for Or in OrderList)+1
+
+
+#Operation          
+print("Your command options: \nPLACE X, Y, (NORTH/EAST/SOUTH/WEST) --> Use this command first\nRIGHT\nLEFT\nMOVE\nREPORT --> Get your current location")
 while True:
-	InputValue = input('What Do You Want To Do: ').upper()
+	InputValue = input("What Do You Want To Do: ").upper()
 	if InputValue == 'QUIT':
 		break
 	if len(InputValue) >= limit:
@@ -42,10 +43,11 @@ while True:
 			if ORDER == 'PLACE':
 				X = clamp(int(X), minn, maxn)
 				Y = clamp(int(Y), minn, maxn)
-		else: print(ErrorMessage)
+		else: 
+			print(ErrorMessage)
 	if len(InputValue) <= limit:
 		ORDER = InputValue.strip().upper()
-		if ORDER in OrderList:
+		if (ORDER in OrderList):
 			if ORDER == 'LEFT':
 				F = TurnLeft[F]
 			if ORDER == 'RIGHT':
@@ -54,4 +56,5 @@ while True:
 				X, Y, F = MakeMove(X, Y, F)
 			if ORDER == 'REPORT':
 				print('Your current coordinate is: ',X,Y,F)
-		else: print(ErrorMessage)
+		elif (ORDER not in OrderList): 
+			print(ErrorMessage)
